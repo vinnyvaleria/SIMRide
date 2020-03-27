aacf71229ba6d1d30477613fedcb1599fdf6f85d
utyumba <vvi001@mymail.sim.edu.sg>
2020-03-28 02:08:11 +0800

    Styling edit 1

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Alert } from 'react-native';
+import { Link } from 'react-router-dom';
+import logo from './assets/logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>yo bro it is working</Text>
+      <Text style={{ color: '#fff', fontSize: 30, fontFamily: 'Helvetica', fontWeight: '600'}} >Welcome to SIMRide</Text>
+      <Image source={logo} /> 
+      
+      <View style={styles.fixToText}>
+          <Button title="Right button" onPress={() => Alert.alert('Right button pressed')} />
+      </View>
     </View>
   );
 }
@@ -12,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#4b9ea1',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
   },
 });
